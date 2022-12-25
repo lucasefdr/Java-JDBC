6e46b9da70c54080d1b300b232d92969a8bc7846
Lucas <lucas.efdrosa@gmail.com>
2022-12-25 17:46:13 -0300

    try with resources

--- src/main/java/io/github/lucasefdr/TesteInsertComParam.java
+++ src/main/java/io/github/lucasefdr/TesteInsertComParam.java
@@ -10,36 +10,37 @@ public class TesteInsertComParam {
     public static void main(String... args) throws SQLException {
 
         ConnectionFactory connectionFactory = new ConnectionFactory();
-        Connection connection = connectionFactory.connection();
 
-        // Controlando as transações não automáticas
-        connection.setAutoCommit(false);
+        // try with resources
+        try (Connection connection = connectionFactory.connection()) {
 
-        try {
-            PreparedStatement preparedStatement = connection
+            // Controlando as transações não automáticas
+            connection.setAutoCommit(false);
+
+            try (PreparedStatement preparedStatement = connection
                     .prepareStatement("INSERT INTO produto (nome, descricao, valor) VALUES (?, ?, ?)",
-                            Statement.RETURN_GENERATED_KEYS);
+                            Statement.RETURN_GENERATED_KEYS)) {
 
-            adicionarVariavel("SmartTV", "40 polegadas", 2340.0D, preparedStatement);
-            adicionarVariavel("Notebook", "Lenovo S45", 3540.0D, preparedStatement);
+                adicionarVariavel("SmartTV", "40 polegadas", 2340.0D, preparedStatement);
+                adicionarVariavel("Notebook", "Lenovo S45", 3540.0D, preparedStatement);
 
-            connection.commit();
-        } catch (Exception e) {
-            e.printStackTrace();
-            System.out.println("ROLLBACK EXECUTADO");
-            connection.rollback();
+                connection.commit();
+            } catch (Exception e) {
+                e.printStackTrace();
+                System.out.println("ROLLBACK EXECUTADO");
+                connection.rollback();
+            }
+
+            try (ResultSet rs = connection.prepareStatement("SELECT * FROM produto ORDER BY id").executeQuery()) {
+
+                while (rs.next()) {
+                    String nomeProduto = rs.getString("nome");
+                    Integer idProduto = rs.getInt("id");
+
+                    System.out.println("ID: " + idProduto + " - Nome: " + nomeProduto);
+                }
+            }
         }
-
-        ResultSet rs = connection.prepareStatement("SELECT * FROM produto ORDER BY id").executeQuery();
-
-        while (rs.next()) {
-            String nomeProduto = rs.getString("nome");
-            Integer idProduto = rs.getInt("id");
-
-            System.out.println("ID: " + idProduto + " - Nome: " + nomeProduto);
-        }
-
-        rs.close();
     }
 
     public static void adicionarVariavel(String nome, String descricao, Double valor, PreparedStatement pstm)
@@ -48,8 +49,8 @@ public class TesteInsertComParam {
         pstm.setString(2, descricao);
         pstm.setDouble(3, valor);
 
-        // if (nome == "Notebook")
-        // throw new Exception("Error");
+        if (nome == "Notebook")
+            throw new Exception("Error");
 
         pstm.execute();
     }
