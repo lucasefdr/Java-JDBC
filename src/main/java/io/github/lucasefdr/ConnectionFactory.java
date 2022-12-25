6b7cd67c89bd5485e57e35b8659dc37e04252b5e
Lucas <lucas.efdrosa@gmail.com>
2022-12-25 18:59:48 -0300

    Criando o pool de conexões

--- src/main/java/io/github/lucasefdr/ConnectionFactory.java
+++ src/main/java/io/github/lucasefdr/ConnectionFactory.java
@@ -4,10 +4,24 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+import javax.sql.DataSource;
+
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+
 // Design pattern: Factory -> fábrica de construção de objetos
 public class ConnectionFactory {
+    public DataSource dataSource;
+
+    public ConnectionFactory() {
+        ComboPooledDataSource comboPooledDataSource = new ComboPooledDataSource();
+        comboPooledDataSource.setJdbcUrl("jdbc:oracle:thin:@localhost:1521/XEPDB1");
+        comboPooledDataSource.setUser("kneladev");
+        comboPooledDataSource.setPassword("kneladev");
+
+        this.dataSource = comboPooledDataSource;
+    }
+
     public Connection connection() throws SQLException {
-        return DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521/XEPDB1", "kneladev",
-                "kneladev");
+        return this.dataSource.getConnection();
     }
 }
